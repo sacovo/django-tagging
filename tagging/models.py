6550f6c04c0d2d67049e8cc3263623811207c66d
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2021-12-12 18:26:03 +0100

    Add support for Django 4 compatibility

--- tagging/models.py
+++ tagging/models.py
@@ -5,6 +5,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection
 from django.db import models
+from django.db.models.query_utils import Q
 from django.utils.encoding import smart_str
 from django.utils.translation import gettext_lazy as _
 
@@ -155,8 +156,9 @@ class TagManager(models.Manager):
             filters = {}
 
         queryset = model._default_manager.filter()
-        for f in filters.items():
-            queryset.query.add_filter(f)
+        for k, v in filters.items():
+            # Add support for both Django 4 and inferior versions
+            queryset.query.add_q(Q((k, v)))
         usage = self.usage_for_queryset(queryset, counts, min_count)
 
         return usage
